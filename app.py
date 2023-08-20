35989d9df00a21bb44aeaccedf88750221daebbc
rayhan-rafin <rayhan.rafin2@gmail.com>
2023-08-20 21:52:02 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+# creates app obj with flask class, __name__ gives the file unq name
+app = Flask(__name__)
+
+# decorator indicates browser endpoint
+
+# '/' for the homepage
+
+
+@app.route('/')
+def homepage():
+    return ("this is the homepage")
+
+
+# hello page
+@app.route('/hello')
+def hello():
+    return 'Hello, Im Rayhan'
+
+
+# hosting locally; why port 5000?
+app.run(host='localhost', port=5000)
